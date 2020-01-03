fd65879bb368fbeb89def758d13217be04ec8e05
Slavisha84 <slavoljub.petkovic@outlook.com>
2020-01-03 18:36:30 -0500

    Update with CHO and PRO

--- app/src/main/java/com/example/metapall/BMRActivity.java
+++ app/src/main/java/com/example/metapall/BMRActivity.java
@@ -1,12 +1,17 @@
 package com.example.metapall;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.DatabaseUtils;
 import android.os.Bundle;
+import android.telephony.SignalStrength;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextClock;
 import android.widget.TextView;
@@ -25,12 +30,23 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
     public static final String ACT_TEXT = "com.metaboli.application.metaboli.ACT_TEXT";
     public static final String BMR_R = "com.metaboli.application.metaboli.BMR_R";
     public static final String TDEE_R = "com.metaboli.application.metaboli.TDEE_R";
+    public static final String MinCHO_R = "com.metaboli.application.metaboli.MinCHO_R";
+    public static final String MaxCHO_R = "com.metaboli.application.metaboli.MaxCHO_R";
+    public static final String PROT_1 = "com.metaboli.application.metaboli.PROT_1";
+    public static final String PROT_2 = "com.metaboli.application.metaboli.PROT_2";
+    public static final String PROT_3 = "com.metaboli.application.metaboli.PROT_3";
 
     private EditText height;
     private EditText weight;
     private EditText age;
+    private EditText duration;
     private TextView BMRResults;
     private TextView TDEEResults;
+    private TextView MinCHOResults;
+    private TextView MaxChOResults;
+    private TextView PROResults1;
+    private TextView PROResults2;
+    private TextView PROResults3;
     private Spinner Gspinner;
     private Spinner Aspinner;
     private String genderCh;
@@ -38,6 +54,9 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
 
     DatabaseHelper PersonalDB;
     Button btnAddData;
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
 
 
     @Override
@@ -49,6 +68,26 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
         age = (EditText) findViewById(R.id.age);
         BMRResults = (TextView) findViewById(R.id.BMRResults);
         TDEEResults = (TextView) findViewById(R.id.TDEEResults);
+        MinCHOResults = (TextView) findViewById((R.id.MinCHOResults));
+        MaxChOResults = (TextView) findViewById((R.id.MaxCHOResults));
+        PROResults1 = (TextView) findViewById(R.id.PROResults1);
+        PROResults2 = (TextView) findViewById(R.id.PROResults2);
+        PROResults3 = (TextView) findViewById(R.id.PROResults3);
+        duration = (EditText)  findViewById(R.id.duration);
+
+
+        // Use Radiobutton to establish the presence of duration field
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.training){
+                    duration.setVisibility(View.VISIBLE);
+                } else {
+                    duration.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
         //Identify spinners view based on r.id
         Gspinner = (Spinner) findViewById(R.id.spinnerGender);
@@ -98,6 +137,13 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
 
         String bmrres = BMRResults.getText().toString();
         String tdeeres = TDEEResults.getText().toString();
+        String minchores = MinCHOResults.getText().toString();
+        String maxchores = MaxChOResults.getText().toString();
+        String protres1 = PROResults1.getText().toString();
+        String protres2 = PROResults2.getText().toString();
+        String protres3 = PROResults3.getText().toString();
+
+
 
         Intent intent = new Intent(this,DashboardActivity.class);
         intent.putExtra(H_TEXT,Height);
@@ -107,11 +153,14 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
         intent.putExtra(ACT_TEXT,ActLvl);
         intent.putExtra(BMR_R,bmrres);
         intent.putExtra(TDEE_R,tdeeres);
+        intent.putExtra(MinCHO_R,minchores);
+        intent.putExtra(MaxCHO_R,maxchores);
+        intent.putExtra(PROT_1,protres1);
+        intent.putExtra(PROT_2,protres2);
+        intent.putExtra(PROT_3,protres3);
         startActivity(intent);
 
     }
-
-
 
     public void AddData(){
         btnAddData.setOnClickListener(new View.OnClickListener() {
@@ -163,12 +212,20 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
         String GENDER = genderCh;
         String AGE = age.getText().toString();
         String ACTIVITY = ActvCh;
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+        String ACTYPE = (String) radioButton.getText();
+        Toast.makeText(this, "Selected Radio Button: " + ACTYPE,Toast.LENGTH_SHORT).show();
+        String DURATION = duration.getText().toString();
+
+
 
         // if statement to execute if all required values are present
-        if (HEIGHT != null && !"".equals(HEIGHT)&& WEIGHT != null && !"".equals(WEIGHT) && AGE != null && !"".equals(AGE) && GENDER != "Gender" && !"".equals(GENDER) && ACTIVITY != "Activity Level" && !"".equals(ACTIVITY)){
+        if (ACTYPE.equals("Training") && HEIGHT != null && !"".equals(HEIGHT)&& WEIGHT != null && !"".equals(WEIGHT) && AGE != null && !"".equals(AGE) && GENDER != "Gender" && !"".equals(GENDER) && ACTIVITY != "Activity Level" && !"".equals(ACTIVITY)){
             double heightValue = Double.parseDouble(HEIGHT);
             double weightValue = Double.parseDouble(WEIGHT);
             double ageValue = Double.parseDouble(AGE);
+            double durValue = Double.parseDouble(DURATION);
 
             // IF statement to provide calculation based on Male gender and one out of 3 level of activity
             if (GENDER .equals("Male"))
@@ -176,6 +233,43 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
                 double bmr = Math.round((((weightValue / 2.2046) * 10) + (6.25 * (heightValue * 30.48)) - (5 * ageValue) + 5) * 100);
                 double bmrr = bmr / 100;
                 BMRResults.setText("BMR: " + bmrr);
+
+                // Calculate CHO for male based on hour of training
+                if (durValue <= 3 ){
+
+                    double mincho = Math.round(((weightValue / 2.2046) * 6) * 100);
+                    double minchor = mincho / 100;
+                    MinCHOResults.setText("Min CHO:" + minchor);
+
+                    double maxcho = Math.round(((weightValue / 2.2046) * 10) * 100);
+                    double maxchor = maxcho / 100;
+                    MaxChOResults.setText("Max CHO:" + maxchor);
+                }
+                else if (durValue >= 4) {
+                    double mincho = Math.round(((weightValue / 2.2046) * 8) * 100);
+                    double minchor = mincho / 100;
+                    MinCHOResults.setText("Min CHO:" + minchor);
+
+                    double maxcho = Math.round(((weightValue / 2.2046) * 12) * 100);
+                    double maxchor = maxcho / 100;
+                    MaxChOResults.setText("Max CHO:" + maxchor);
+                }
+
+
+                double pro1 = Math.round(((weightValue / 2.2046) * 1.2) * 100);
+                double pror1 = pro1 / 100;
+
+                double pro2 = Math.round(((weightValue / 2.2046) * 1.4) * 100);
+                double pror2 = pro2 / 100;
+
+                double pro3 = Math.round(((weightValue / 2.2046) * 2.2) * 100);
+                double pror3 = pro3 / 100;
+                PROResults1.setText(("PRO: " + pror1));
+                PROResults2.setText(("PRO: " + pror2));
+                PROResults3.setText(("PRO: " + pror3));
+
+
+
                 if (ACTIVITY .equals("Light"))
                 {
                     double dtee = Math.round((bmrr * 1.53) * 100);
@@ -197,6 +291,40 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
             {
                 double bmr = Math.round((((weightValue / 2.2046) * 10) + (6.25 * (heightValue * 30.48)) - (5 * ageValue) - 161) * 100);
                 double bmrr = bmr / 100;
+
+                // Calculate CHO for female based on hours of training
+                if (durValue <= 3 ){
+
+                    double mincho = Math.round(((weightValue / 2.2046) * 6) * 100);
+                    double minchor = mincho / 100;
+                    MinCHOResults.setText("Min CHO:" + minchor);
+
+                    double maxcho = Math.round(((weightValue / 2.2046) * 10) * 100);
+                    double maxchor = maxcho / 100;
+                    MaxChOResults.setText("Max CHO:" + maxchor);
+                }
+                else if (durValue >= 4) {
+                    double mincho = Math.round(((weightValue / 2.2046) * 8) * 100);
+                    double minchor = mincho / 100;
+                    MinCHOResults.setText("Min CHO:" + minchor);
+
+                    double maxcho = Math.round(((weightValue / 2.2046) * 12) * 100);
+                    double maxchor = maxcho / 100;
+                    MaxChOResults.setText("Max CHO:" + maxchor);
+                }
+
+                double pro1 = Math.round(((weightValue / 2.2046) * 1.2) * 100);
+                double pror1 = pro1 / 100;
+
+                double pro2 = Math.round(((weightValue / 2.2046) * 1.4) * 100);
+                double pror2 = pro2 / 100;
+
+                double pro3 = Math.round(((weightValue / 2.2046) * 2.2) * 100);
+                double pror3 = pro3 / 100;
+                PROResults1.setText(("PRO: " + pror1));
+                PROResults2.setText(("PRO: " + pror2));
+                PROResults3.setText(("PRO: " + pror3));
+
                 BMRResults.setText("BMR: " + bmrr);
                 if (ACTIVITY .equals("Light"))
                 {
@@ -215,6 +343,106 @@ public class BMRActivity extends AppCompatActivity implements AdapterView.OnItem
                 }
             }
         }
+
+        if (ACTYPE.equals("Competition") && HEIGHT != null && !"".equals(HEIGHT)&& WEIGHT != null && !"".equals(WEIGHT) && AGE != null && !"".equals(AGE) && GENDER != "Gender" && !"".equals(GENDER) && ACTIVITY != "Activity Level" && !"".equals(ACTIVITY)){
+            double heightValue = Double.parseDouble(HEIGHT);
+            double weightValue = Double.parseDouble(WEIGHT);
+            double ageValue = Double.parseDouble(AGE);
+
+            // IF statement to provide calculation based on Male gender and one out of 3 level of activity
+            if (GENDER .equals("Male"))
+            {
+                double bmr = Math.round((((weightValue / 2.2046) * 10) + (6.25 * (heightValue * 30.48)) - (5 * ageValue) + 5) * 100);
+                double bmrr = bmr / 100;
+                BMRResults.setText("BMR: " + bmrr);
+
+                // Calculate CHO for male based on hour of training
+                double mincho = Math.round(((weightValue / 2.2046) * 10) * 100);
+                double minchor = mincho / 100;
+                MinCHOResults.setText("Min CHO:" + minchor);
+
+                double maxcho = Math.round(((weightValue / 2.2046) * 8) * 100);
+                double maxchor = maxcho / 100;
+                MaxChOResults.setText("Max CHO:" + maxchor);
+
+
+
+                double pro1 = Math.round(((weightValue / 2.2046) * 1.2) * 100);
+                double pror1 = pro1 / 100;
+
+                double pro2 = Math.round(((weightValue / 2.2046) * 1.4) * 100);
+                double pror2 = pro2 / 100;
+
+                double pro3 = Math.round(((weightValue / 2.2046) * 2.2) * 100);
+                double pror3 = pro3 / 100;
+                PROResults1.setText(("PRO: " + pror1));
+                PROResults2.setText(("PRO: " + pror2));
+                PROResults3.setText(("PRO: " + pror3));
+
+
+
+                if (ACTIVITY .equals("Light"))
+                {
+                    double dtee = Math.round((bmrr * 1.53) * 100);
+                    TDEEResults.setText("TDEE: " + dtee / 100);
+                }
+                else if (ACTIVITY .equals("Moderate"))
+                {
+                    double dtee = Math.round((bmrr * 1.76) * 100);
+                    TDEEResults.setText("TDEE: " + dtee / 100);
+                }
+                else if (ACTIVITY .equals("Vigorous"))
+                {
+                    double dtee = Math.round((bmrr * 2.25) * 100);
+                    TDEEResults.setText("TDEE: " + dtee / 100);
+                }
+            }
+            // IF statement to provide calculation based on Female gender and one out of 3 level of activity
+            else if (GENDER .equals("Female"))
+            {
+                double bmr = Math.round((((weightValue / 2.2046) * 10) + (6.25 * (heightValue * 30.48)) - (5 * ageValue) - 161) * 100);
+                double bmrr = bmr / 100;
+
+                // Calculate CHO for male based on hour of training
+                double mincho = Math.round(((weightValue / 2.2046) * 10) * 100);
+                double minchor = mincho / 100;
+                MinCHOResults.setText("Min CHO:" + minchor);
+
+                double maxcho = Math.round(((weightValue / 2.2046) * 8) * 100);
+                double maxchor = maxcho / 100;
+                MaxChOResults.setText("Max CHO:" + maxchor);
+
+                double pro1 = Math.round(((weightValue / 2.2046) * 1.2) * 100);
+                double pror1 = pro1 / 100;
+
+                double pro2 = Math.round(((weightValue / 2.2046) * 1.4) * 100);
+                double pror2 = pro2 / 100;
+
+                double pro3 = Math.round(((weightValue / 2.2046) * 2.2) * 100);
+                double pror3 = pro3 / 100;
+                PROResults1.setText(("PRO: " + pror1));
+                PROResults2.setText(("PRO: " + pror2));
+                PROResults3.setText(("PRO: " + pror3));
+
+                BMRResults.setText("BMR: " + bmrr);
+                if (ACTIVITY .equals("Light"))
+                {
+                    double dtee = Math.round((bmrr * 1.53) * 100);
+                    TDEEResults.setText("TDEE: " + dtee / 100);
+                }
+                else if (ACTIVITY .equals("Moderate"))
+                {
+                    double dtee = Math.round((bmrr * 1.76) * 100);
+                    TDEEResults.setText("TDEE: " + dtee / 100);
+                }
+                else if (ACTIVITY .equals("Vigorous"))
+                {
+                    double dtee = Math.round((bmrr * 2.25) * 100);
+                    TDEEResults.setText("TDEE: " + dtee / 100);
+                }
+            }
+        }
+
         else
             {
             Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/metapall/DashboardActivity.java
+++ app/src/main/java/com/example/metapall/DashboardActivity.java
@@ -19,7 +19,12 @@ public class DashboardActivity extends AppCompatActivity {
         String ACTtext = intent.getStringExtra(BMRActivity.ACT_TEXT);
         String BRM_RES = intent.getStringExtra(BMRActivity.BMR_R);
         String TDEE_RES = intent.getStringExtra(BMRActivity.TDEE_R);
+        String MIN_CHO_RES = intent.getStringExtra(BMRActivity.MinCHO_R);
+        String MAX_CHO_RES = intent.getStringExtra(BMRActivity.MaxCHO_R);
 
+        String PRO1 = intent.getStringExtra(BMRActivity.PROT_1);
+        String PRO2 = intent.getStringExtra(BMRActivity.PROT_2);
+        String PRO3 = intent.getStringExtra(BMRActivity.PROT_3);
 
         TextView HEIGHT = (TextView) findViewById(R.id.HEIGHT);
         TextView WEIGHT = (TextView) findViewById(R.id.WEIGHT);
@@ -28,8 +33,11 @@ public class DashboardActivity extends AppCompatActivity {
         TextView ACTVLVL = (TextView) findViewById(R.id.ACTLVL);
         TextView BMRResults = (TextView) findViewById(R.id.BMRResults);
         TextView TDEEResults = (TextView) findViewById(R.id.TDEEResults);
-
-
+        TextView MinCHO = (TextView) findViewById(R.id.MinCHO_RES);
+        TextView MaxCHO = (TextView) findViewById(R.id.MaxCHO_RES);
+        TextView Pro1 = (TextView) findViewById(R.id.PROTEIN1);
+        TextView Pro2 = (TextView) findViewById(R.id.PROTEIN2);
+        TextView Pro3 = (TextView) findViewById(R.id.PROTEIN3);
 
         HEIGHT.setText("HEIGHT:           " + Htext + " ft");
         WEIGHT.setText("WEIGHT:           " + Wtext + " lb");
@@ -38,6 +46,11 @@ public class DashboardActivity extends AppCompatActivity {
         ACTVLVL.setText("ACTIVITY LVL:  " + ACTtext);
         BMRResults.setText(BRM_RES);
         TDEEResults.setText(TDEE_RES);
+        MinCHO.setText(MIN_CHO_RES);
+        MaxCHO.setText(MAX_CHO_RES);
+        Pro1.setText(PRO1);
+        Pro2.setText(PRO2);
+        Pro3.setText(PRO3);
 
 
     }
