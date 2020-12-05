110d8c003ba2a2b121534759da1436984c57d79e
Slavisha84 <slavoljub.petkovic@outlook.com>
2020-12-04 23:45:42 -0500

    Added  signin screen

---
+++ app/src/main/java/com/example/metapall/Signin.java
@@ -0,0 +1,32 @@
+package com.example.metapall;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.os.Bundle;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        ImageView imageView3 = findViewById(R.id.imageView3);
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), BMRActivity.class);
+                startActivity(activity2Intent);
+            }
+        });
+
+    }
+}
